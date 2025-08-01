a316d06681eb87f06dd60683db4429fa4f6a8beb
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2025-08-01 01:06:06 +0100

    Fixing global reference

--- src/test.ts
+++ src/test.ts
@@ -1,1 +1,6 @@
-console.log("Hello World")
+import { fileURLToPath } from 'url';
+import path from "path"
+import {Example} from "./Example.js"
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
